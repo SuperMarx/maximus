8c3999ae4bcd40825c0502fe4951b56874a75bd8
Wouter Geraedts <git@woutergeraedts.nl>
2016-02-23 11:48:27 +0100

    Adapted Maximus to use downloader_manager

--- src/maximus/scraper.cpp
+++ src/maximus/scraper.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <deque>
 #include <fstream>
+#include <functional>
 #include <boost/locale.hpp>
 
 #include <supermarx/util/stubborn.hpp>
@@ -15,11 +16,38 @@ namespace supermarx
 	scraper::scraper(product_callback_t _product_callback, tag_hierarchy_callback_t, unsigned int _ratelimit, bool _cache, bool)
 	: product_callback(_product_callback)
 	, dl("supermarx maximus/1.0", _ratelimit, _cache ? boost::optional<std::string>("./cache") : boost::none)
+	, m(dl, [&]() { error_count++; })
+	, product_count(0)
+	, page_count(0)
+	, error_count(0)
 	{}
 
 	void scraper::scrape()
 	{
-		const std::string base_uri = "http://www.jumbo.com";
+		product_count = 0;
+		page_count = 0;
+		error_count = 0;
+
+		std::function<void(std::string, size_t)> schedule_f([this, &schedule_f](std::string const& curi, size_t i) -> void {
+			page_count++;
+
+			std::string puri = curi + "?PageNumber=" + boost::lexical_cast<std::string>(i);
+			m.schedule(puri, [=](downloader::response const& response) {
+				size_t product_i = 0;
+				product_parser pp([&](const message::product_base& p, boost::optional<std::string> const& image_uri, datetime retrieved_on, confidence conf, problems_t probs)
+				{
+					product_callback(puri, image_uri, p, retrieved_on, conf, probs);
+
+					++product_i;
+					++product_count;
+				});
+
+				pp.parse(response.body);
+
+				if(product_i > 0 && product_i < 100)
+					schedule_f(curi, i+1);
+			});
+		});
 
 		category_parser cp([&](category_parser::category_uri_t const& _curi)
 		{
@@ -30,26 +58,12 @@ namespace supermarx
 				return;
 
 			std::string curi = what[1];
-
-			for(size_t i = 0; i < 100; ++i)
-			{
-				std::string puri = curi + "?PageNumber=" + boost::lexical_cast<std::string>(i);
-
-				size_t product_i = 0;
-				product_parser pp([&](const message::product_base& p, boost::optional<std::string> const& image_uri, datetime retrieved_on, confidence conf, problems_t probs)
-				{
-					product_callback(puri, image_uri, p, retrieved_on, conf, probs);
-
-					++product_i;
-				});
-				pp.parse(dl.fetch(puri).body);
-
-				if(product_i == 0)
-					return;
-			}
+			schedule_f(curi, 0);
 		});
 
-		cp.parse(dl.fetch(base_uri + "/producten").body);
+		cp.parse(dl.fetch("http://www.jumbo.com/producten").body);
+		m.process_all();
+		std::cerr << "Pages: " << page_count << ", products: " << product_count << ", errors: " << error_count << std::endl;
 	}
 
 	raw scraper::download_image(const std::string& uri)
--- src/maximus/scraper.hpp
+++ src/maximus/scraper.hpp
@@ -2,6 +2,7 @@
 
 #include <supermarx/raw.hpp>
 #include <supermarx/util/cached_downloader.hpp>
+#include <supermarx/util/download_manager.hpp>
 
 #include <supermarx/scraper/scraper_prototype.hpp>
 
@@ -18,6 +19,9 @@ namespace supermarx
 		product_callback_t product_callback;
 
 		cached_downloader dl;
+		download_manager m;
+
+		size_t product_count, page_count, error_count;
 
 	public:
 		scraper(product_callback_t _product_callback, tag_hierarchy_callback_t _tag_hierarchy_callback, unsigned int ratelimit = 5000, bool cache = false, bool register_tags = false);
